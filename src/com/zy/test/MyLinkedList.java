ac158116dd0d417b21051223c2172d8023ae0766
ZhaoYing <zhaoying1258@qq.com>
2015-07-31 10:15:43 +0800

    MyLinkedList8.0 增加：判断两个链表是否相交；若相交，如何找出第一个相交的结点

--- src/com/zy/test/MyLinkedList.java
+++ src/com/zy/test/MyLinkedList.java
@@ -237,6 +237,73 @@ public class MyLinkedList {
 		return true;
 	}
 	
+	/**
+	 * 如何判断两个链表是否相交
+	 * @param h1:链表h1的结点
+	 * @param h2:链表h2的结点
+	 * @return:相交，返回true
+	 */
+	public boolean isIntersect(Node h1, Node h2){
+		if(h1==null || h2==null)
+			return false;
+		Node tail1 = h1;
+		//找到链表h1的尾结点
+		while(tail1.next!=null)
+			tail1 = tail1.next;
+		Node tail2 = h2;
+		//找到链表h2的尾结点
+		while(tail2.next!=null){
+			tail2 = tail2.next;
+		}
+		return tail1 == tail2;
+	}
+	/**
+	 * 若两个链表相交，找出他们相交的第一个结点
+	 */
+	public static Node getFirstMeetNode(Node h1, Node h2 ){
+		if(h1==null || h2==null)
+			return null;
+		Node tail1 = h1;
+		int len1 = 1;
+		//找到链表h1的尾结点
+		while(tail1.next!=null){
+			tail1 = tail1.next;
+			len1++;
+		}
+		Node tail2 = h2;
+		int len2 = 1;
+		//扎到链表h2的尾结点
+		while(tail2.next!=null){
+			tail2 = tail2.next;
+			len2++;
+		}
+		//两链表不想交
+		if(tail1!=tail2)
+			return null;
+		Node t1 = h1;
+		Node t2 = h2;
+		//找出较长的链表，先遍历
+		if(len1>len2){
+			int d = len1-len2;
+			while(d!=0){
+				t1 = t1.next;
+				d--;
+			}
+		}
+		else{
+			int d = len2-len1;
+			while(d!=0){
+				t2 = t2.next;
+				d--;
+			}
+		}
+		while(t1!=t2){
+			t1 = t1.next;
+			t2 = t2.next;
+		}
+		return t1;
+		
+	}
 	
 	
 	
