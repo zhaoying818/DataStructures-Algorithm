1828e5de60d0aeca07a0f933f163bf23b5f6ea07
ZhaoYing <zhaoying1258@qq.com>
2015-08-03 15:44:54 +0800

    Sort4.0 增加MergeSort算法

--- src/com/zy/test/Sort.java
+++ src/com/zy/test/Sort.java
@@ -67,12 +67,54 @@ public class Sort {
 				}
 	}
 	
+	/**
+	 * MergeSortπÈ≤¢≈≈–Ú
+	 */
+	public static void MergeSort(int[] a, int p, int r){
+		if(p<r){
+			int q = (p+r)/2;
+			MergeSort(a, p, q);
+			MergeSort(a, q+1, r);
+			Merge(a, p, q, r);
+		}
+	}
+	public static void Merge(int a[], int p ,int q, int r){
+		int i,j,k,n1,n2;
+		n1 = q-p+1;
+		n2 = r-q;
+		int[] L = new int[n1];
+		int[] R = new int[n2];
+		for(i=0,k=p;i<n1;i++,k++)
+			L[i] = a[k];
+		for(i=0,k=q+1;i<n2;i++,k++)
+			R[i] = a[k];
+		for(k=p,i=0,j=0;i<n1&&j<n2;k++){
+			if(L[i]<R[j]){
+				a[k] = L[i];
+				i++;
+			}
+			else{
+				a[k] = R[j];
+				j++;
+			}
+		}
+		if(i<n1){
+			for(j=i;j<n1;j++,k++)
+				a[k] = L[j];
+		}
+		if(j<n2){
+			for(i=j;i<n2;i++,k++)
+				a[k] = R[i];
+		}
+	}
+	
+	
 	
 	
 	
 	public static void main(String[] args){
 		int a[] = {36,25,48,12,25,65,43,57};
-		BubbleSort(a);
+		MergeSort(a,0,a.length-1);
 		for(int i=0;i<a.length;i++){
 			System.out.print(a[i] + " ");
 		}
