16bfa83002f650ecd63f4d3f48673e218d615c66
ZhaoYing <zhaoying1258@qq.com>
2015-08-03 15:06:20 +0800

    Sort2.0 增加insertSort插入排序算法

--- src/com/zy/test/Sort.java
+++ src/com/zy/test/Sort.java
@@ -30,6 +30,29 @@ public class Sort {
 		}
 	}
 	
+	
+	/**
+	 * insertSort插入排序
+	 * @param a:要排序的数组
+	 */
+	public static void insertSort(int[] a){
+		if(a!=null){
+			for(int i=1;i<a.length;i++){
+				int temp = a[i];
+				int j=i;
+				if(a[j-1]>temp){
+					while(j>=1 && a[j-1]>temp){
+						a[j] = a[j-1];
+						j--;
+					}
+				}
+				a[j]=temp;
+			}
+		}
+	}
+	
+	
+	
 	public static void main(String[] args){
 		int a[] = {36,25,48,12,25,65,43,57};
 		selectSort(a);
