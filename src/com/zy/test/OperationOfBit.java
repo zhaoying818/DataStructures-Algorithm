17b9b53dd6c56358e50a1a2f4580f38f4f4dbac9
ZhaoYing <zhaoying1258@qq.com>
2015-08-03 17:58:07 +0800

    Bit Operation2.0 判断一个数是否是2的n次方

--- src/com/zy/test/OperationOfBit.java
+++ src/com/zy/test/OperationOfBit.java
@@ -11,10 +11,25 @@ public class OperationOfBit {
 		return m;
 	}
 	
+	/**
+	 * 判断一个数是否是2的n次方
+	 * @param n: 输入的数
+	 * @return: 是，返回true;否则，返回false
+	 */
+	public static boolean isPower(int n){
+		if(n<1) return false;
+		int m = n&(n-1);
+		return m==0;
+	}
 	
+//	public static void main(String[] args){
+//		System.out.println("3乘以8(3乘以2的3次方)为: " +powerN(3, 3));
+//	}
 	
 	public static void main(String[] args){
-		System.out.println("3乘以8(3乘以2的3次方)为: " +powerN(3, 3));
+		System.out.println("4是2的n次方:" + isPower(4));
+		System.out.println("5是2的n次方:" + isPower(5));
 	}
+
 
 }
