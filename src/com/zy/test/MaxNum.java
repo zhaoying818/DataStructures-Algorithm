f00d76e248c9d89e7d80d12c5a46de3dec5d8dcb
ZhaoYing <zhaoying1258@qq.com>
2015-08-06 11:04:28 +0800

    Array_MaxNum 用递归求一个整数数组的最大元素

---
+++ src/com/zy/test/MaxNum.java
@@ -0,0 +1,23 @@
+package com.zy.test;
+
+public class MaxNum {
+
+	public static void main(String[] args) {
+		MaxNum mn = new MaxNum();
+		int a[] = {0,16,2,3,4,5,10,7,8,9,23};
+		System.out.println(mn.maxNum(a,0));
+	}
+	
+	public int maxNum(int a[],int begin){
+		int len = a.length-begin;
+		if(len==1)
+			return a[begin];
+		else{
+			return max(a[begin],maxNum(a,begin+1));
+		}
+	}
+	private int max(int a, int b){
+		return a>b? a:b;
+	}
+
+}
